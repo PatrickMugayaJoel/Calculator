6df8b38d425e600372918dcb827789137e076a12
Mugaya Joel Patrick <josean.patrick11@gmail.com>
2019-09-28 11:35:34 +0300

    Creating a template
    Added css and html to the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="side-pannel full-height">
+            <span className="title">Numbers</span>
+            <div className="form">
+              <form>
+                <label>Number one</label>
+                <input type="number" name="num-one" className="input"/>
+                <label>Number two</label>
+                <input type="number" name="num-two" className="input"/>
+                <label>Operation</label>
+                <select type="text" name="operation" className="input">
+                  <option value="add">ADD</option>
+                  <option value="sub">SUB</option>
+                  <option value="mul">MUL</option>
+                  <option value="div">DIV</option>
+                </select>
+                <input type="submit" className="submit" value="Post"/>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <div className="results-pannel full-height">
+            <span className="title">Results</span>
+            <div className="table">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Number&nbsp;1</th>
+                    <th>Number&nbsp;2</th>
+                    <th>Response</th>
+                    <th>Expected</th>
+                    <th>Passed</th>
+                    <th>Remove</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="wrong">
+                    <td>4</td>
+                    <td>5</td>
+                    <td>778</td>
+                    <td>9</td>
+                    <td>No</td>
+                    <td>x</td>
+                  </tr>
+                  <tr className="correct">
+                    <td>4</td>
+                    <td>5</td>
+                    <td>9</td>
+                    <td>9</td>
+                    <td>Yes</td>
+                    <td>x</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+      </div>
+        </div>
     </div>
   );
 }
